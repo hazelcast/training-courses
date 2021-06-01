5f26754e5fb5748618ba3812e46b332b3fcac304
berk <berk@hazelcast.com>
2021-06-01 14:15:19 +0200

    MapStore store sample comments updated

--- In_Memory_Storage_Essentials/lab5-MapStore/src/main/java/com/hazelcast/ClientStore.java
+++ In_Memory_Storage_Essentials/lab5-MapStore/src/main/java/com/hazelcast/ClientStore.java
@@ -14,20 +14,20 @@ public class ClientStore {
         // Create a Hazelcast backed map
         IMap<Integer, String> training = client.getMap("training");
 
-        System.out.println("MapStore un loadAllKeys i ilk cagrildiktan sonraki kayit sayisi: " + training.size());
+        System.out.println("Initial size of the map after loadAllKeys is invoked " + training.size());
 
         training.set(20, "value20");
         training.set(21, "value21");
         training.set(22, "value22");
 
-        System.out.println("Yeni kayitlar eklendikten sonraki kayit sayisi: " + training.size());
+        System.out.println("Size of the map after writing adding entries: " + training.size());
 
         training.evictAll();
 
-        System.out.println("Eviction sonrasi kayit sayisi: " + training.size());
+        System.out.println("Size of the map after eviction: " + training.size());
 
         training.loadAll(true);
 
-        System.out.println("Database e write-through yazilan kayitlar loadAll yapildiginda: " + training.size());
+        System.out.println("Size of the map after the entries that are written to persistent data store (write-through) are loaded again (via loadAll(true)): " + training.size());
     }
 }
